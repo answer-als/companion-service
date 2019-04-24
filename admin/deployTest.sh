942c3bb1c12f00cede544a6be27bf3c9aec169b7
Jay Beavers <jay@hikinghomeschoolers.org>
2019-04-23 22:45:09 -0700

    Update docker to map host filesystem to api containers

--- admin/deployTest.sh
+++ admin/deployTest.sh
@@ -3,4 +3,4 @@ docker build -t answer-als/companion-service:test .
 docker stop test-companion-service
 docker rm test-companion-service
 docker system prune -f
-docker run -d --name test-companion-service --restart always -e "CONTAINER_NAME=test-companion-service" -l traefik.frontend.rule=Host:answeralsdev.evergreencircuits.com/test -l traefik.enable=true answer-als/companion-service:test
+docker run -d --name test-companion-service --restart always -v /data/companionservicetest:/data/companionservice -e "CONTAINER_NAME=test-companion-service" -l traefik.frontend.rule=Host:test.answeralsdev.evergreencircuits.com-l traefik.enable=true answer-als/companion-service:test
