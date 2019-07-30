b8201ff658180f03b47d8f98df89d477be250a39
Jay Beavers <jay.beavers@evergreencircuits.com>
2019-07-30 09:38:58 -0700

    Fixed up to have the correct test URL

--- admin/deployTest.sh
+++ admin/deployTest.sh
@@ -3,4 +3,4 @@ docker build -t answer-als/companion-service:test .
 docker stop test-companion-service
 docker rm test-companion-service
 docker system prune -f
-docker run -d --name test-companion-service --restart always -v /data/companionservicetest:/data/companionservice -e AZURE_STORAGE_ACCOUNT -e AZURE_STORAGE_ACCESS_KEY -e "CONTAINER_NAME=test-companion-service" -l traefik.frontend.rule=Host:test.answeralsdev.evergreencircuits.com -l traefik.enable=true answer-als/companion-service:test
+docker run -d --name test-companion-service --restart always -v /data/companionservicetest:/data/companionservice -e AZURE_STORAGE_ACCOUNT -e AZURE_STORAGE_ACCESS_KEY -e "CONTAINER_NAME=test-companion-service" -l traefik.frontend.rule=Host:answeralsdev.evergreencircuits.com -l traefik.enable=true answer-als/companion-service:test
