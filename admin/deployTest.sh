538ff28271daffdbb8badf73a8186008bc620950
Barrett M. Conrad <bconrad@cotingasoft.com>
2021-08-17 19:27:27 -0500

    Consolitating docker build parameters into docker-compose.yml

--- admin/deployTest.sh
+++ admin/deployTest.sh
@@ -1,6 +1,6 @@
-export DOCKER_HOST=ssh://azure-test
-docker build -t answer-als/companion-service:test .
-docker stop test-companion-service
-docker rm test-companion-service
+export DOCKER_HOST=ssh://aals-test
+docker build -t answer-als/companion-service .
+docker stop companion-service
+docker rm companion-service
 docker system prune -f
-docker run -d --name test-companion-service --restart always -p 3000:3000 -v /data/companionservicetest:/data/companionservice -e AZURE_STORAGE_ACCOUNT -e AZURE_STORAGE_ACCESS_KEY -e "CONTAINER_NAME=test-companion-service" -l traefik.port=3000 -l traefik.foo.frontend.rule=Host:service-dev.answerals.org -l traefik.bar.frontend.rule=Host:service.answerals.org -l traefik.enable=true answer-als/companion-service:test
+docker run -d --restart always answer-als/companion-service
