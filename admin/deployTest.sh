11fcd1e75ea515bcdd9de22c99fe5f3227ad41ef
Barrett M. Conrad <bconrad@cotingasoft.com>
2021-08-17 19:40:59 -0500

    Remove volume from docker-compose

--- admin/deployTest.sh
+++ admin/deployTest.sh
@@ -3,4 +3,4 @@ docker build -t answer-als/companion-service .
 docker stop companion-service
 docker rm companion-service
 docker system prune -f
-docker run -d --restart always answer-als/companion-service
+docker run -d --restart always -v /data/companionservicetest:/data/companionservice answer-als/companion-service
