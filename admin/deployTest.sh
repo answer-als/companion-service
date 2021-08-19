00125fc304d224260908b4a8ce4906e3b1df167d
Barrett M. Conrad <bconrad@cotingasoft.com>
2021-08-18 23:41:40 -0500

    Utilize .env file in docker run for test deploy script;

--- admin/deployTest.sh
+++ admin/deployTest.sh
@@ -7,5 +7,6 @@ docker system prune -f
 docker run -d --name companion-service \
   --restart always \
   -p 3000:3000 \
+  --env-file .env \
   -l traefik.port=3000 -l traefik.foo.frontend.rule=Host:service-dev.answerals.org -l traefik.bar.frontend.rule=Host:service.answerals.org -l traefik.enable=true \
   companion-service
