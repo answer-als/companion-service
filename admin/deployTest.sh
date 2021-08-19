fe577f266648a99f2770fa5ffcd5839cbbc538dd
Barrett M. Conrad <bconrad@cotingasoft.com>
2021-08-18 21:16:57 -0500

    Refine deploy script;

--- admin/deployTest.sh
+++ admin/deployTest.sh
@@ -1,7 +1,11 @@
-docker-compose build --no-cache --progress=plain
+#!/bin/bash
 export DOCKER_HOST=ssh://aals-test
-#docker build -t answer-als/companion-service .
+docker build --no-cache -t companion-service .
 docker stop companion-service
 docker rm companion-service
 docker system prune -f
-docker run -d --name companion-service --restart always -l traefik.port=3000 -l traefik.foo.frontend.rule=Host:service-dev.answerals.org -l traefik.bar.frontend.rule=Host:service.answerals.org -l traefik.enable=true
+docker run -d --name companion-service \
+  --restart always \
+  -p 3000:3000 \
+  -l traefik.port=3000 -l traefik.foo.frontend.rule=Host:service-dev.answerals.org -l traefik.bar.frontend.rule=Host:service.answerals.org -l traefik.enable=true \
+  companion-service
