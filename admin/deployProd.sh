696cfb5f055c9ef90314e3d6190811f3465c33f8
Barrett M. Conrad <bconrad@cotingasoft.com>
2021-09-16 09:34:24 -0500

    Cleaning up deployment scripts;

--- admin/deployProd.sh
+++ admin/deployProd.sh
@@ -1,11 +1,19 @@
-#eval $(docker-machine env vm-prod)
-#export DOCKER_HOST=ssh://answerals-prod
+#!/bin/bash
 export DOCKER_HOST=ssh://vm-prod
-#export DOCKER_HOST=ssh://andrew@52.149.153.225
 tag=$(date '+%Y-%m-%d')-prod
-#docker tag answer-als/companion-service:test answer-als/companion-service:$tag
 docker build -t answer-als/companion-service:$tag .
 docker stop companion-service
 docker rm companion-service
 docker system prune -f
-docker run -d --name companion-service --restart always -p 3000:3000 -v /data/companionservice:/data/companionservice -e AZURE_STORAGE_ACCOUNT_PROD -e AZURE_STORAGE_ACCESS_KEY_PROD -e "CONTAINER_NAME=companion-service" -l traefik.port=3000 -l traefik.foo.frontend.rule=Host:service-prod.answerals.org -l traefik.bar.frontend.rule=Host:service.answerals.org -l traefik.enable=true answer-als/companion-service:$tag
+docker run -d --name companion-service \
+  --restart always \
+  -p 3000:3000 \
+  -v /data/companionservice:/data/companionservice \
+  -e AZURE_STORAGE_ACCOUNT_PROD \
+  -e AZURE_STORAGE_ACCESS_KEY_PROD \
+  -e "CONTAINER_NAME=companion-service" \
+  -l traefik.port=3000 \
+  -l traefik.foo.frontend.rule=Host:service-prod.answerals.org \
+  -l traefik.bar.frontend.rule=Host:service.answerals.org \
+  -l traefik.enable=true \
+  answer-als/companion-service:$tag
--- admin/deployTest.sh
+++ admin/deployTest.sh
@@ -7,6 +7,11 @@ docker system prune -f
 docker run -d --name companion-service \
   --restart always \
   -p 3000:3000 \
+  -v /data/companionservice:/data/companionservice \
   --env-file .env \
-  -l traefik.port=3000 -l traefik.foo.frontend.rule=Host:service-dev.answerals.org -l traefik.bar.frontend.rule=Host:service.answerals.org -l traefik.enable=true \
+  -e "CONTAINER_NAME=companion-service" \
+  -l traefik.port=3000 \
+  -l traefik.foo.frontend.rule=Host:service-dev.answerals.org \
+  -l traefik.bar.frontend.rule=Host:service.answerals.org \
+  -l traefik.enable=true \
   companion-service
